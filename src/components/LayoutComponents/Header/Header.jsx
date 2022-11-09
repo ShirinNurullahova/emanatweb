d4e1d3d5f1f6cb21bed023273e686d8c64be3950
ShirinNurullahova <90415981+ShirinNurullahova@users.noreply.github.com>
2022-11-09 10:23:28 +0400

    Update Header.jsx

--- src/components/LayoutComponents/Header/Header.jsx
+++ src/components/LayoutComponents/Header/Header.jsx
@@ -1,27 +1,27 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.scss'
 import Logo from '../../../assets/img/logo.svg'
 import SearchIcon from '../../../assets/img/search.svg'
 import Modal from '../Modal/Modal'
 const Header = () => {
-  const [menuOpen , setMenuOpen] = useState(false);
-    
-  const toggle= () =>{
-      if(menuOpen){
-        setMenuOpen(false)
-          // console.log("salam")
-      }else{
-       setMenuOpen(true)
-        // console.log("sagol")
-      }
-   }
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggle = () => {
+    if (menuOpen) {
+      setMenuOpen(false)
+      // console.log("salam")
+    } else {
+      setMenuOpen(true)
+      // console.log("sagol")
+    }
+  }
   return (
     <header>
       <div className="header-container">
         <div className="header-container-logo">
           <Link to='/'>
-          <img src={Logo} alt="emanat-logo" loading="lazy"/>
+            <img src={Logo} alt="emanat-logo" loading="lazy" />
 
           </Link>
         </div>
@@ -30,23 +30,34 @@ const Header = () => {
           <label>
             <input type="text" />
 
-            <img onClick={toggle} src={SearchIcon} alt="emanat-axtaris" loading="lazy"/>
+            <img onClick={toggle} src={SearchIcon} alt="emanat-axtaris" loading="lazy" />
           </label>
         </div>
 
         <nav className='header-container-navbar'>
           <ul>
-          <li><Link to='/about'>Haqqımızda</Link></li>
-          {/* <li><Link to={'/coorperative'}>Terminal xəritəsi</Link></li> */}
-          <li><Link to={'/service'}>Əməkdaşlıq</Link></li>
-          <li><Link to='/career'>Karyera</Link></li>
-          <li><Link to={'/contact'}>Əlaqə</Link></li>
-          <li><Link>AZ</Link></li>
+            <li>
+              <Link to='/about'>Haqqımızda</Link>
+            </li>
+            {/* <li><Link to={'/coorperative'}>Terminal xəritəsi</Link></li> */}
+            <li className='header-container-navbar-li'>
+              <Link to={'/service'}>Əməkdaşlıq</Link>
+              <div className='header-container-navbar-li-div'>
+                <p>xeber</p>
+                <p>vakansiya</p>
+                <p>istifade</p>
+
+              </div>
+
+            </li>
+            <li><Link to='/career'>Karyera</Link></li>
+            <li><Link to={'/contact'}>Əlaqə</Link></li>
+            <li><Link>AZ</Link></li>
 
           </ul>
         </nav>
       </div>
-      <Modal menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Modal menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </header>
   )
 }
